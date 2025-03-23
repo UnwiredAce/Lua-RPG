e46238675a0fbd1374b088f45ae8a79a47237b1a
siddharth <siddhumyname@gmail.com>
2025-03-23 13:34:04 +0530

    added tilemap, physics and movement with camera setup for the level

--- main.lua
+++ main.lua
@@ -1,15 +1,24 @@
+local screenWidth = love.graphics.getWidth()
+local screenHeight = love.graphics.getHeight()
+
 function love.load()
+    wf = require('libraries/windfield')
+    world = wf.newWorld()
+
     camera = require('libraries/camera')
     cam = camera()
     
     anim8 = require('libraries/anim8')
+
+    sti = require('libraries/sti')
+    gamemap = sti('sprites/maps/tileset.lua')
 
     love.graphics.setDefaultFilter("nearest", "nearest")
 
     player = {}
     player.x = 300
     player.y = 200
-    player.speed = 500
+    player.speed = 100
 
     player.spriteIdle = love.graphics.newImage('sprites/idle.png')
     player.gridIdle = anim8.newGrid(48, 64, player.spriteIdle:getWidth(), player.spriteIdle:getHeight())
@@ -36,27 +45,36 @@ function love.load()
 
     player.spriteSheet = player.spriteIdle
     player.anim = player.animation.idle
+
+    player.collision = world:newBSGRectangleCollider(40, 350, 10, 40, 3)
+    player.collision:setFixedRotation(true)
 end
 
 function love.update(dt)
     local moving = false
+    local velX = 0
+    local velY = 0
 
     if love.keyboard.isDown("w") then
+        velY = player.speed * -1
         player.spriteSheet = player.spriteWalk
         player.anim = player.animation.walk.up
         player.lastDirection = "up"
         moving = true
     elseif love.keyboard.isDown("a") then
+        velX = player.speed * -1
         player.spriteSheet = player.spriteWalk
         player.anim = player.animation.walk.left
         player.lastDirection = "left"
         moving = true
     elseif love.keyboard.isDown("s") then
+        velY = player.speed
         player.spriteSheet = player.spriteWalk
         player.anim = player.animation.walk.down
         player.lastDirection = "down"
         moving = true
     elseif love.keyboard.isDown("d") then
+        velX = player.speed
         player.spriteSheet = player.spriteWalk
         player.anim = player.animation.walk.right
         player.lastDirection = "right"
@@ -70,11 +88,47 @@ function love.update(dt)
 
     player.anim:update(dt)
     cam:lookAt(player.x, player.y)
+
+    player.collision:setLinearVelocity(velX, velY)
+    world:update(dt)
+    player.x = player.collision:getX()
+    player.y = player.collision:getY()
+    local mapW = gamemap.width * gamemap.tilewidth
+    local mapH = gamemap.height * gamemap.tileheight
+
+    local minX = mapW - (screenWidth * 0.66)
+    local maxX = mapW - (screenWidth * 0.18)
+
+    local minY = mapH - (screenHeight * -0.56)
+    local maxY = mapH - (screenHeight * -0.83)
+    
+    if cam.x < minX then
+        cam.x = minX
+    end
+
+    if cam.x > maxX then
+        cam.x = maxX
+    end
+
+    if cam.y < minY then
+        cam.y = minY
+    end
+
+    if cam.y > maxY then
+        cam.y = maxY
+    end
+
+    --cam.y = player.y + 150
 end
 
 function love.draw()
     cam:attach()
-    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 3, nil, 24, 30)
+    gamemap:drawLayer(gamemap.layers['BaseRoad'])
+    gamemap:drawLayer(gamemap.layers['Base'])
+    gamemap:drawLayer(gamemap.layers['Details'])
+    gamemap:drawLayer(gamemap.layers['Props'])
+    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 1.5, nil, 25, 32)
+    --world:draw()
     cam:detach()
 end
 
